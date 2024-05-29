b17fd5aa48f456d85b2dcfbe4110ed9103e15669
PinyaGOOO <165675370+PinyaGOOO@users.noreply.github.com>
2024-05-29 20:01:16 +0300

    Add files via upload

--- hq-srv.sh
+++ hq-srv.sh
@@ -2,6 +2,7 @@
 dnf remove -y git
 dnf install -y nftables
 dnf install -y bind bind-utils
+dnf install -y chrony
 
 useradd -c "Admin" Admin -U
 echo "Admin:P@ssw0rd" | chpasswd
@@ -19,13 +20,28 @@ sed -i "s/listen-on port 53 { 127.0.0.1; };/listen-on { any; };/" /etc/named.con
 sed -i "s/allow-query     { localhost; };/allow-query     { any; };/" /etc/named.conf
 sed -i '18a\\tforward first;' /etc/named.conf
 sed -i '19a\\tforwarders { 8.8.8.8; 77.88.4.4; };' /etc/named.conf
-echo -e 'zone "hq.work" {\n\ttype master;\n\tfile "hq.db";\n};\nzone "branch.work" {\n\ttype master;\n\tfile "branch.db";\n};\nzone "100.16.172.in-addr.arpa" {\n\ttype master;\n\tfile "172.db";\n};\nzone "100.168.192.in-addr.arpa" {\n\ttype master;\n\tfile "192.db";\n};' >> /etc/named.conf
+echo -e 'zone "hq.work" {\n\ttype master;\n\tfile "/etc/zone/hq.db";\n};\nzone "branch.work" {\n\ttype master;\n\tfile "/etc/zone/branch.db";\n};\nzone "100.16.172.in-addr.arpa" {\n\ttype master;\n\tfile "/etc/zone/172.db";\n};\nzone "100.168.192.in-addr.arpa" {\n\ttype master;\n\tfile "/etc/zone192.db";\n};' >> /etc/named.conf
 mkdir /etc/zone
 echo -e "\$TTL\t1D\n@\tIN\tSOA\thq.work root.hq.work. (\n\t\t\t\t2024021400\t; serial\n\t\t\t\t12H\t\t; refresh\n\t\t\t\t1H\t\t; retry\n\t\t\t\t1W\t\t; expire\n\t\t\t\t1H\t\t; ncache\n\t\t\t)\n\tIN\tNS\thq.work.\n\tIN\tA\t127.0.0.0\nhq-r\tIN\tA\t172.16.100.1\nhq-srv\tIN\tA\t172.16.100.2" >/etc/zone/hq.db
 echo -e "\$TTL\t1D\n@\tIN\tSOA\tbranch.work root.branch.work. (\n\t\t\t\t2024021400\t; serial\n\t\t\t\t12H\t\t; refresh\n\t\t\t\t1H\t\t; retry\n\t\t\t\t1W\t\t; expire\n\t\t\t\t1H\t\t; ncache\n\t\t\t)\n\tIN\tNS\tbranch.work.\n\tIN\tA\t127.0.0.0\nbr-r\tIN\tA\t192.168.100.1\nbr-srv\tIN\tA\t192.168.100.10" >/etc/zone/branch.db
 echo -e "\$TTL\t1D\n@\tIN\tSOA\thq.work root.hq.work. (\n\t\t\t\t2024021400\t; serial\n\t\t\t\t12H\t\t; refresh\n\t\t\t\t1H\t\t; retry\n\t\t\t\t1W\t\t; expire\n\t\t\t\t1H\t\t; ncache\n\t\t\t)\n\tIN\tNS\thq.work.\n1\tIN\tPTR\thq-r.hq.work.\n2\tIN\tPTR\thq-srv.hq.work." >/etc/zone/172.db
 echo -e "\$TTL\t1D\n@\tIN\tSOA\tbranch.work root.branch.work. (\n\t\t\t\t2024021400\t; serial\n\t\t\t\t12H\t\t; refresh\n\t\t\t\t1H\t\t; retry\n\t\t\t\t1W\t\t; expire\n\t\t\t\t1H\t\t; ncache\n\t\t\t)\n\tIN\tNS\tbranch.work.\n1\tIN\tPTR\tbr-r.branch.work." >/etc/zone/192.db
 chown root:named /etc/zone/{hq,branch,172,192}.db
+
+systemctl enable --now named
+systemctl restart named
+systemctl restart NetworkManager
+
+timedatectl set-timezone Europe/Moscow
+sed -i '3s/^/#/' /etc/chrony.conf
+sed -i '4s/^/#/' /etc/chrony.conf
+sed -i '5s/^/#/' /etc/chrony.conf
+sed -i '6s/^/#/' /etc/chrony.conf
+sed -i '7a\server 172.16.100.1 iburst prefer' /etc/chrony.conf
+
+systemctl enable --now chronyd
+systemctl restart chronyd
+chronyc sources
 
 hostnamectl set-hostname HQ-SRV; exec bash
 
